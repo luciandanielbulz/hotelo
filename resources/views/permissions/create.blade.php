1c52e61105ce52706dd8215982f505a4123784f5
Lucian Bulz <office@bulz.at>
2024-12-10 12:21:43 +0100

    Permission Styling angepasst

--- resources/views/permissions/create.blade.php
+++ resources/views/permissions/create.blade.php
@@ -1,33 +1,27 @@
 <x-layout>
-    <div class="grid grid-cols-1 gap-x-8 gap-y-8 pt-10 md:grid-cols-3">
+    <div class="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-gray-900/10 pb-12 md:grid-cols-3">
         <!-- Linke Spalte: Überschrift -->
         <div class="px-4 sm:px-0">
-            <h2 class="text-base font-semibold text-gray-900">Neues Recht anlegen</h2>
+            <h2 class="text-base font-semibold text-gray-900">Recht bearbeiten</h2>
             <p class="mt-1 text-sm text-gray-600">Bitte füllen Sie die folgenden Informationen aus, um ein neues Recht anzulegen.</p>
         </div>
 
         <!-- Formular -->
-        <form action="{{ route('permissions.store') }}" method="POST" class="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2">
+        <form action="{{ route('permissions.store') }}" method="POST">
             @csrf
-            <div class="px-4 py-6 sm:p-8">
-                <div class="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <!-- Rechtname -->
-                    <div class="sm:col-span-3">
-                        <label for="name" class="block text-sm font-medium text-gray-900">Rechtname</label>
-                        <div class="mt-2">
-                            <input type="text" name="name" id="name" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-indigo-600">
-                        </div>
-                    </div>
-
-                    <!-- Beschreibung -->
-                    <div class="sm:col-span-3">
-                        <label for="description" class="block text-sm font-medium text-gray-900">Beschreibung</label>
-                        <div class="mt-2">
-                            <input type="text" name="description" id="description" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-indigo-600">
-                        </div>
-                    </div>
+            <div class="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2 md:col-span-2">
+                <!-- Rollenname -->
+                <div class="sm:col-span-2">
+                    <x-input name="name" type="text" placeholder="Name" label="Name" value="" />
                 </div>
             </div>
+            <div class="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2 md:col-span-2">
+                <!-- Rollenname -->
+                <div class="sm:col-span-2">
+                    <x-input name="description" type="text" placeholder="Beschreibung" label="Beschreibung" value="" />
+                </div>
+            </div>
+
 
             <!-- Schaltflächen -->
             <div class="flex items-center justify-end gap-x-6 border-t border-gray-900/10 px-4 py-4 sm:px-8">
--- resources/views/permissions/edit.blade.php
+++ resources/views/permissions/edit.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <div class="grid grid-cols-1 gap-x-8 gap-y-8 pt-10 md:grid-cols-3">
+    <div class="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-gray-900/10 pb-12 md:grid-cols-3">
         <!-- Linke Spalte: Überschrift -->
         <div class="px-4 sm:px-0">
             <h2 class="text-base font-semibold text-gray-900">Recht bearbeiten</h2>
@@ -8,28 +8,22 @@
 
         <!-- Formular -->
 
-        <form action="{{ route('permissions.update', $permissions->id) }}" method="POST" class="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2">
+        <form action="{{ route('permissions.update', $permissions->id) }}" method="POST">
             @csrf
             @method('PUT')
-            <div class="px-4 py-6 sm:p-8">
-                <div class="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <!-- Rechtname -->
-                    <div class="sm:col-span-3">
-                        <label for="name" class="block text-sm font-medium text-gray-900">Rechtname</label>
-                        <div class="mt-2">
-                            <input type="text" name="name" id="name" value="{{ $permissions->name }}" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-indigo-600">
-                        </div>
-                    </div>
-
-                    <!-- Beschreibung -->
-                    <div class="sm:col-span-3">
-                        <label for="description" class="block text-sm font-medium text-gray-900">Beschreibung</label>
-                        <div class="mt-2">
-                            <input type="text" name="description" id="description" value="{{ $permissions->description }}" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-indigo-600">
-                        </div>
-                    </div>
+            <div class="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2 md:col-span-2">
+                <!-- Rollenname -->
+                <div class="sm:col-span-2">
+                    <x-input name="name" type="text" placeholder="Name" label="Name" value="{{  $permissions->name  }}" />
                 </div>
             </div>
+            <div class="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2 md:col-span-2">
+                <!-- Rollenname -->
+                <div class="sm:col-span-2">
+                    <x-input name="description" type="text" placeholder="Beschreibung" label="Beschreibung" value="{{  $permissions->description  }}" />
+                </div>
+            </div>
+
 
             <!-- Schaltflächen -->
             <div class="flex items-center justify-end gap-x-6 border-t border-gray-900/10 px-4 py-4 sm:px-8">
