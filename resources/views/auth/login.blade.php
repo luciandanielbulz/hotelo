5937e53a54d7ca3624b6795b78bc3cbe3a889f87
Lucian Bulz <office@bulz.at>
2024-12-24 14:19:50 +0100

    loginfenster angepasst

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,9 +33,6 @@
                 <div>
                     <div class="flex items-center justify-between">
                         <label for="password" class="block text-sm font-medium text-gray-900">Password</label>
-                        <div class="text-sm">
-                            <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
-                        </div>
                     </div>
                     <div class="mt-2">
                         <input
@@ -58,10 +55,7 @@
                 </div>
             </form>
 
-            <p class="mt-10 text-center text-sm text-gray-500">
-                Not a member?
-                <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
-            </p>
+
         </div>
     </div>
 </x-guest-layout>
--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -124,8 +124,8 @@
 
 
             <!-- Einstellungen Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
-                <x-dropdown align="right" width="48">
+            <div class="hidden sm:flex sm:items-center sm:ml-6 md:ml-20">
+                <x-dropdown align="right" width="60">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->name }}</div>
