e0c87ce67811b39169e2c028835352505832c382
Lucian Bulz <office@bulz.at>
2025-06-13 12:11:58 +0200

    bankdata tabelle

--- database/migrations/2025_01_01_156554_create_bankdata_table.php
+++ database/migrations/2025_01_01_156554_create_bankdata_table.php
@@ -6,27 +6,35 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
-        Schema::create('bankdata', function (Blueprint $table) {
-            $table->id();
-            $table->date('date');
-            $table->string('partnername');
-            $table->string('partneriban')->nullable();
-            $table->string('partnerbic')->nullable();
-            $table->string('partneracount')->nullable();
-            $table->string('partnerroutingnumber')->nullable();
-            $table->decimal('amount', 10, 2);
-            $table->string('currency', 3);
-            $table->text('reference')->nullable();
-            $table->string('referencenumber')->nullable();
-            $table->foreignId('client_id')->constrained()->onDelete('cascade');
-            $table->enum('category', [1, 2]);
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('bankdata')) {
+            Schema::create('bankdata', function (Blueprint $table) {
+                $table->id();
+                $table->date('date');
+                $table->string('partnername');
+                $table->string('partneriban')->nullable();
+                $table->string('partnerbic')->nullable();
+                $table->string('partneracount')->nullable();
+                $table->string('partnerroutingnumber')->nullable();
+                $table->decimal('amount', 10, 2);
+                $table->string('currency', 3);
+                $table->text('reference')->nullable();
+                $table->string('referencenumber')->nullable();
+                $table->foreignId('client_id')->constrained();
+                $table->enum('category', ['1', '2']);
+                $table->timestamps();
+            });
+        }
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::dropIfExists('bankdata');
     }
