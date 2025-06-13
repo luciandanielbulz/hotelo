ee8ab0668ff7e6e92596a1c0cd2284d67dae6b98
Lucian Bulz <office@bulz.at>
2025-06-13 11:09:02 +0200

    User anlegen Bugfix

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -18,44 +18,45 @@
         </div>
 
         <!-- Formular -->
-        <form action="{{ route('users.create') }}" method="GET" class="sm:col-span-1 md:col-span-5 ">
+        <form action="{{ route('users.store') }}" method="POST" class="sm:col-span-1 md:col-span-5 ">
+            @csrf
             <div class="grid md:grid-cols-4 sm:grid-cols-1 pb-4 gap-x-6">
                 <!-- Vorname -->
                 <div class="sm:col-span-1">
-                    <x-input name="name" type="text" placeholder="Name" label="Name" value="" />
+                    <x-input name="name" type="text" placeholder="Name" label="Name" value="{{ old('name') }}" />
                 </div>
 
                 <!-- Nachname -->
                 <div class="sm:col-span-1">
-                    <x-input name="lastname" type="text" placeholder="Nachname" label="Nachname" value="" />
+                    <x-input name="lastname" type="text" placeholder="Nachname" label="Nachname" value="{{ old('lastname') }}" />
                 </div>
             </div>
 
             <div class="grid md:grid-cols-4 sm:grid-cols-1 pb-4 gap-x-6 border-b">
                 <!-- Loginname -->
                 <div class="sm:col-span-1">
-                    <x-input name="login" type="text" placeholder="Loginname" label="Loginname" value="" />
+                    <x-input name="username" type="text" placeholder="Loginname" label="Loginname" value="{{ old('username') }}" />
                 </div>
 
                 <!-- Klient -->
                 <div class="sm:col-span-1">
-                    <x-dropdown_body name="client_id" id="client_id" :options="$clients->pluck('clientname', 'id')" :selected="1" label="Klient" placeholder="Bitte auswählen" class="w-full appearance-none rounded-md bg-white py-1.5 pl-3 pr-8 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm" />
+                    <x-dropdown_body name="client_id" id="client_id" :options="$clients->pluck('clientname', 'id')" :selected="old('client_id', 1)" label="Klient" placeholder="Bitte auswählen" class="w-full appearance-none rounded-md bg-white py-1.5 pl-3 pr-8 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm" />
                 </div>
             </div>
             <div class="grid md:grid-cols-4 sm:grid-cols-1 pb-4 gap-x-6 border-b">
                 <!-- Email -->
                 <div class="sm:col-span-1">
-                    <x-input name="lemailogin" type="text" placeholder="Email" label="E-Mail" value="" />
+                    <x-input name="email" type="text" placeholder="Email" label="E-Mail" value="{{ old('email') }}" />
                 </div>
 
                 <!-- Aktiv -->
                 <div class="sm:col-span-1">
-                    <x-dropdown_body name="isactive" id="isactive" :options="['1' => 'Ja', '0' => 'Nein']" :selected="1" label="Aktiv" placeholder="Bitte auswählen" class="w-full appearance-none rounded-md bg-white py-1.5 pl-3 pr-8 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm" />
+                    <x-dropdown_body name="isactive" id="isactive" :options="['1' => 'Ja', '0' => 'Nein']" :selected="old('isactive', '1')" label="Aktiv" placeholder="Bitte auswählen" class="w-full appearance-none rounded-md bg-white py-1.5 pl-3 pr-8 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm" />
                 </div>
 
                 <!-- Rolle -->
                 <div class="sm:col-span-1">
-                    <x-dropdown_body name="role_id" id="role_id" :options="$roles->pluck('name', 'id')" :selected="1" label="Rolle" placeholder="Bitte auswählen" />
+                    <x-dropdown_body name="role_id" id="role_id" :options="$roles->pluck('name', 'id')" :selected="old('role_id', 1)" label="Rolle" placeholder="Bitte auswählen" />
                 </div>
 
             </div>
