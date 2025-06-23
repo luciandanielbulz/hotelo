e5cc8e2bf1e78c5a61b113c553c0d345aa10010c
Lucian Bulz <office@bulz.at>
2025-06-23 13:38:51 +0200

    Kleine Anpassungen

--- app/Http/Controllers/PdfCreateController.php
+++ app/Http/Controllers/PdfCreateController.php
@@ -76,7 +76,7 @@ class PdfCreateController extends Controller
             $font = $fontMetrics->get_font('helvetica');
             $size = 10;
             $pageText = 'Seite ' . $pageNumber . ' von ' . $pageCount;
-            $y = 710;
+            $y = 740;
             $x = 500;
             $canvas->text($x, $y, $pageText, $font, $size, array(0.5, 0.5, 0.5));
         });
@@ -450,7 +450,7 @@ class PdfCreateController extends Controller
         body { font-family:  Roboto, sans-serif;font-size: 12px; line-height: 1.2; margin: 0; padding: 0; margin-bottom: 60px; }
         .header { width: 100%; position: relative; height: 110px; margin-bottom: 30px; }
         .logo { position: absolute; top: 0; left: 0; height: 40px; max-width: 150px; }
-        .company-info { position: absolute; top: 135px; left: 0; font-size: 8px; color: black; }
+        .company-info { position: absolute; top: 135px; left: 0; font-size: 9px; color: black; }
         
         .document-info { position: absolute; top: 150px; right: 0; width: 250px; text-align: right;}
         .document-info table { width: 100%; border-collapse: collapse; font-size: 10px; }
