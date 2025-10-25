ac9364b1203d3ce7d5e411d09d7c6163111773bf
Lucian Bulz <office@bulz.at>
2025-10-25 12:46:16 +0200

    bugfix sidebar, Postausgang

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -128,11 +128,11 @@
                         </li>
                     @endif
 
-                    @if($user && $user->hasPermission('view_messages'))
+                    @if($user && $user->hasPermission('view_email_list'))
                         <li>
-                            <a href="{{ route('outgoingemails.index') }}" class="group flex gap-x-3 rounded-md p-2 text-sm font-semibold text-gray-700 hover:bg-gray-100 hover:text-gray-900">
+                            <a href="{{ route('outgoingemails.index') }}" class="group flex gap-x-3 rounded-md p-2 text-sm font-semibold {{ request()->routeIs('outgoingemails.*') ? 'bg-gray-100 text-gray-900' : 'text-gray-700 hover:bg-gray-100 hover:text-gray-900' }}">
                                 <!-- SVG Icon -->
-                                <svg class="h-6 w-6 shrink-0 text-gray-500 group-hover:text-gray-900" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                <svg class="h-6 w-6 shrink-0 {{ request()->routeIs('outgoingemails.*') ? 'text-gray-700' : 'text-gray-500 group-hover:text-gray-900' }}" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                                 </svg>
                                 Postausgang
