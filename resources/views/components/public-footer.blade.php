9adda9aa8ca49d094f31fa005f7aed85ff1275a0
Lucian Bulz <office@bulz.at>
2026-01-07 09:20:01 +0100

    kleiner fix footer

--- resources/views/components/public-footer.blade.php
+++ resources/views/components/public-footer.blade.php
@@ -1,8 +1,11 @@
 <!-- Footer -->
 <footer class="bg-gray-900 text-gray-300 py-12 px-4 sm:px-6 lg:px-8">
     <div class="max-w-7xl mx-auto">
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-8 text-center">
+        <div class="grid grid-cols-1 md:grid-cols-5 gap-8 text-center">
             
+            <div>
+                
+            </div>
             <div>
                 <ul class="space-y-2 text-sm">
                     <!--<li><a href="#features" class="hover:text-white transition-colors">Features</a></li>-->
@@ -13,9 +16,8 @@
                 </ul>
             </div>
             <div>
+                
                 <ul class="space-y-2 text-sm">
-                    
-                    
                     <li><a href="{{ route('impressum') }}" class="hover:text-white transition-colors">Impressum</a></li>
                 </ul>
             </div>
@@ -24,6 +26,8 @@
                     <li><a href="{{ route('privacy') }}" class="hover:text-white transition-colors">Datenschutz</a></li>
                     
                 </ul>
+            </div>
+            <div>
             </div>
         </div>
         <div class="border-t border-gray-800 mt-8 pt-8 text-center text-sm text-gray-400">
